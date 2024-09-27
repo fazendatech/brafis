507f5bcb061afbb7f1a8e1faac9b13984e2d430e
CayoPOliveira <cayoproliveira@gmail.com>
2024-09-27 00:23:22 -0300

    feat: add CertificateP12 error classes

---
+++ src/certificate/indexErrors.ts
@@ -0,0 +1,22 @@
+// src/errors/CertificateErrors.ts
+
+export class NoBagsFoundError extends Error {
+  constructor(bagType: string) {
+    super(`No bags of type ${bagType} found in the PFX file.`);
+    this.name = "NoBagsFoundError";
+  }
+}
+
+export class NoPrivateKeyFoundError extends Error {
+  constructor() {
+    super("No private key found in the PFX file.");
+    this.name = "NoPrivateKeyFoundError";
+  }
+}
+
+export class NoCertificatesFoundError extends Error {
+  constructor() {
+    super("No valid certificates found in the PFX file.");
+    this.name = "NoCertificatesFoundError";
+  }
+}
