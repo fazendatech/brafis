9119b0c09ce6e7eb74513052a839f03b8ec4b0a7
gabrieljablonski <contact@gabrieljablonski.com>
2024-10-01 16:09:22 -0300

    test: add test for non-error object

--- src/utils/errors.test.ts
+++ src/utils/errors.test.ts
@@ -2,7 +2,7 @@ import { describe, test, expect } from "bun:test";
 import { errorHasMessage } from "@/utils";
 
 describe("errorHasMessage", () => {
-  test("should return true if the error has the expected message", () => {
+  test("Returns true if the error has the expected message", () => {
     const error = new Error("Error message");
     const message = "Error message";
 
@@ -11,9 +11,18 @@ describe("errorHasMessage", () => {
     expect(result).toBe(true);
   });
 
-  test("should return false if the error has a different message", () => {
+  test("Returns false if the error has a different message", () => {
     const error = new Error("Error message");
     const message = "Another error message";
+
+    const result = errorHasMessage(error, message);
+
+    expect(result).toBe(false);
+  });
+
+  test("Returns false if the error is not an instance of Error", () => {
+    const error = "Error message";
+    const message = "Error message";
 
     const result = errorHasMessage(error, message);
 
