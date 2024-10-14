07b48dfb6f62fd175e804208d748dfeeac3c33e9
CayoPOliveira <cayoproliveira@gmail.com>
2024-10-13 23:54:05 -0300

    chore: improve uf-ufcode functions

--- src/dfe/nfe/types.d.ts
+++ src/dfe/nfe/types.d.ts
@@ -1,6 +1,11 @@
 import type { LiteralStringUnion } from "@/utils/types";
 
-export type Environment = "produção" | "homologação";
+/**
+ * @description Ambiente de emissão da NF-e (tpAmb).
+ * producao -> tpAmb = 1
+ * homologacao -> tpAmb = 2
+ */
+export type Environment = "producao" | "homologacao";
 
 export type UF = LiteralStringUnion<
   | "AC"
@@ -32,6 +37,42 @@ export type UF = LiteralStringUnion<
   | "TO"
 >;
 
+/**
+ * @description Código numérico de uma UF
+ */
+export type UFCode = LiteralStringUnion<
+  | "12"
+  | "27"
+  | "16"
+  | "13"
+  | "29"
+  | "23"
+  | "53"
+  | "32"
+  | "52"
+  | "21"
+  | "51"
+  | "50"
+  | "31"
+  | "15"
+  | "25"
+  | "41"
+  | "26"
+  | "22"
+  | "33"
+  | "24"
+  | "43"
+  | "11"
+  | "14"
+  | "42"
+  | "35"
+  | "28"
+  | "17"
+>;
+
+/**
+ * @description Servidores de autorização da NF-e
+ */
 export type AuthServer =
   | "AM"
   | "BA"
@@ -58,3 +99,24 @@ export type WebService =
   | "NFeAutorizacao"
   | "NFeRetAutorizacao"
   | "NFeDistribuicaoDFe";
+
+export type UrlWebServices = {
+  [env in Environment]: {
+    [server in AuthServer]: {
+      [service in WebService]?: string;
+    };
+  };
+};
+
+export type GetWebServiceUrlOptions = {
+  uf?: UF;
+  service: WebService;
+  env: Environment;
+  contingency?: boolean;
+};
+
+export type ConsultaCadastroOptions = {
+  IE?: string;
+  CNPJ?: string;
+  CPF?: string;
+};
