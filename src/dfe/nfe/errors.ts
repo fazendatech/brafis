a260ca992117110746039428e1c20b5f28a9e4de
CayoPOliveira <cayoproliveira@gmail.com>
2024-10-13 23:38:24 -0300

    chore: update errors

--- src/dfe/nfe/errors.ts
+++ src/dfe/nfe/errors.ts
@@ -1,30 +1,51 @@
-import type { UF, Environment, WebService } from "./types.ts";
+/**
+ * @description Não foi localizado a url do webservice.
+ */
 export class WebServiceNotFoundError extends Error {
-  constructor({
-    uf,
-    service,
-    env,
-    contingencia,
-  }: { uf: UF; service: WebService; env: Environment; contingencia: boolean }) {
-    super(
-      `Web service not found for ${service}/${uf} (${env}, contingencia: ${contingencia})`,
-    );
+  constructor() {
+    super("Web service not found");
     this.name = "WebServiceNotFoundError";
   }
 }
 
-export class NfeStatusServiceError extends Error {
+/**
+ * @description Especificar a UF para obter a URL do webservice.
+ */
+export class WebServiceUfError extends Error {
   constructor() {
-    super("Unable to get status from the response");
-    this.name = "NfeStatusServiceError";
+    super("Specify the UF to get the web service URL");
+    this.name = "WebServiceUfError";
   }
 }
 
+/**
+ * @description Tempo limite de uma requisição a um serviço foi excedido.
+ */
+export class ServiceRequestTimeoutError extends Error {
+  constructor() {
+    super("Request timed out");
+    this.name = "ServiceRequestTimeoutError";
+  }
+}
+
+/**
+ * @description Falha ao fazer uma requisição de serviço.
+ */
 export class ServiceRequestError extends Error {
   constructor(error: Error, url: string, xml: string) {
     super(
       `Original Error: ${error.message}\nResquest to ${url} failed with XML:\n${xml.length > 100 ? `${xml.substring(0, 100)}...` : xml}`,
     );
     this.name = "ServiceRequestError";
+  }
+}
+
+/**
+ * @description Falha ao obter o status a partir da resposta do servidor.
+ */
+export class NfeStatusServiceError extends Error {
+  constructor() {
+    super("Unable to get status from the server's response");
+    this.name = "NfeStatusServiceError";
   }
 }
