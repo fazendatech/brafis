65a34e7e99cd9d784641357be0cfaea2e6d24c67
CayoPOliveira <cayoproliveira@gmail.com>
2024-10-17 22:48:09 -0300

    chore: create interface XMLNamespace

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import { errorHasMessage } from "@/utils/errors";
 import { fetchWithTls } from "@/utils/fetch";
-import { buildSoap, parseSoap } from "@/utils/xml";
+import { buildSoap, parseSoap, type XMLNamespace } from "@/utils/xml";
 
 export { errorHasMessage, fetchWithTls, buildSoap, parseSoap };
+export type { XMLNamespace };
