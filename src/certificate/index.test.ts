3b3919b9da14b873c27470c68c6e940c7c02a620
CayoPOliveira <cayoproliveira@gmail.com>
2024-10-21 11:05:27 -0300

    refactor: certificate testing structure

--- src/certificate/index.test.ts
+++ src/certificate/index.test.ts
@@ -21,91 +21,99 @@ describe("CertificateP12", async () => {
     mock.restore();
   });
 
-  test("Convert PFX to PEM format", () => {
-    const certificate = new CertificateP12({
-      pfx: selfSignedPfxBuffer,
-      password,
-    });
-    const pem = certificate.asPem();
+  describe("asPem", () => {
+    test("Convert PFX to PEM format", () => {
+      const certificate = new CertificateP12({
+        pfx: selfSignedPfxBuffer,
+        password,
+      });
+      const pem = certificate.asPem();
 
-    expect(pem.cert).toEqual(selfSignedPemCert);
-    expect(pem.key).toEqual(selfSignedPemKey);
+      expect(pem.cert).toEqual(selfSignedPemCert);
+      expect(pem.key).toEqual(selfSignedPemKey);
+    });
+
+    test("Throws InvalidPfxError", () => {
+      const invalidPfx = new Uint8Array();
+      const certificate = new CertificateP12({ pfx: invalidPfx, password });
+
+      expect(() => certificate.asPem()).toThrowError(InvalidPfxError);
+    });
+
+    test("Throws InvalidPasswordError", () => {
+      const certificate = new CertificateP12({
+        pfx: selfSignedPfxBuffer,
+        password: "wrongpass",
+      });
+
+      expect(() => certificate.asPem()).toThrowError(InvalidPasswordError);
+    });
+
+    test("Throws NoPrivateKeyFoundError", () => {
+      spyOn(forge.pkcs12, "pkcs12FromAsn1").mockReturnValueOnce({
+        version: "",
+        safeContents: [],
+        getBags: () => ({}),
+        getBagsByFriendlyName: () => [],
+        getBagsByLocalKeyId: () => [],
+      });
+
+      const certificate = new CertificateP12({
+        pfx: selfSignedPfxBuffer,
+        password,
+      });
+
+      expect(() => certificate.asPem()).toThrowError(NoPrivateKeyFoundError);
+    });
+
+    test("Throws NoCertificatesFoundError", () => {
+      spyOn(forge.pkcs12, "pkcs12FromAsn1").mockImplementationOnce(
+        (...args) => {
+          // @ts-ignore: Compiler error is pointless
+          const p12 = forge.pkcs12.pkcs12FromAsn1(...args);
+          const getBags = p12.getBags;
+
+          // NOTE: Skip the first two calls used by `getPrivateKey`
+          spyOn(p12, "getBags")
+            .mockImplementationOnce(getBags)
+            .mockImplementationOnce(getBags)
+            .mockImplementationOnce(() => ({}));
+
+          return p12;
+        },
+      );
+
+      const certificate = new CertificateP12({
+        pfx: selfSignedPfxBuffer,
+        password,
+      });
+
+      expect(() => certificate.asPem()).toThrowError(NoCertificatesFoundError);
+    });
   });
 
-  test("Loads PFX from file", async () => {
-    const certificate = await CertificateP12.fromFilepath({
-      filepath: `${selfSignedPath}cert.pfx`,
-      password,
-    });
-    const pem = certificate.asPem();
+  describe("fromFilepath", () => {
+    test("Loads PFX from file", async () => {
+      const certificate = await CertificateP12.fromFilepath({
+        filepath: `${selfSignedPath}cert.pfx`,
+        password,
+      });
+      const pem = certificate.asPem();
 
-    expect(pem.cert).toEqual(selfSignedPemCert);
-    expect(pem.key).toEqual(selfSignedPemKey);
+      expect(pem.cert).toEqual(selfSignedPemCert);
+      expect(pem.key).toEqual(selfSignedPemKey);
+    });
   });
 
-  test("Extracts certificate fields", () => {
-    const certificate = new CertificateP12({
-      pfx: selfSignedPfxBuffer,
-      password,
+  describe("getPemFields", () => {
+    test("Extracts certificate fields", () => {
+      const certificate = new CertificateP12({
+        pfx: selfSignedPfxBuffer,
+        password,
+      });
+      const info = certificate.getPemFields();
+
+      expect(info).toMatchSnapshot();
     });
-    const info = certificate.getPemFields();
-
-    expect(info).toMatchSnapshot();
-  });
-
-  test("Throws InvalidPfxError on invalid PFX file", () => {
-    const invalidPfx = new Uint8Array();
-    const certificate = new CertificateP12({ pfx: invalidPfx, password });
-
-    expect(() => certificate.asPem()).toThrowError(InvalidPfxError);
-  });
-
-  test("Throws InvalidPasswordError on invalid password", () => {
-    const certificate = new CertificateP12({
-      pfx: selfSignedPfxBuffer,
-      password: "wrongpass",
-    });
-
-    expect(() => certificate.asPem()).toThrowError(InvalidPasswordError);
-  });
-
-  test("Throws NoPrivateKeyFoundError if no private key found in PFX file", () => {
-    spyOn(forge.pkcs12, "pkcs12FromAsn1").mockReturnValueOnce({
-      version: "",
-      safeContents: [],
-      getBags: () => ({}),
-      getBagsByFriendlyName: () => [],
-      getBagsByLocalKeyId: () => [],
-    });
-
-    const certificate = new CertificateP12({
-      pfx: selfSignedPfxBuffer,
-      password,
-    });
-
-    expect(() => certificate.asPem()).toThrowError(NoPrivateKeyFoundError);
-  });
-
-  test("Throws NoCertificatesFoundError if no certificates found", () => {
-    spyOn(forge.pkcs12, "pkcs12FromAsn1").mockImplementationOnce((...args) => {
-      // @ts-ignore: Compiler error is pointless
-      const p12 = forge.pkcs12.pkcs12FromAsn1(...args);
-      const getBags = p12.getBags;
-
-      // NOTE: Skip the first two calls used by `getPrivateKey`
-      spyOn(p12, "getBags")
-        .mockImplementationOnce(getBags)
-        .mockImplementationOnce(getBags)
-        .mockImplementationOnce(() => ({}));
-
-      return p12;
-    });
-
-    const certificate = new CertificateP12({
-      pfx: selfSignedPfxBuffer,
-      password,
-    });
-
-    expect(() => certificate.asPem()).toThrowError(NoCertificatesFoundError);
   });
 });
