31c4737806fd3fc14085d059ae86bdcd89e5dde8
gabrieljablonski <contact@gabrieljablonski.com>
2024-10-25 19:38:01 -0300

    feat: `TimeoutError` wrapper class

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -1,3 +1,10 @@
 export function errorHasMessage(e: unknown, message: string): e is Error {
   return e instanceof Error && e.message === message;
 }
+
+export class TimeoutError extends Error {
+  constructor() {
+    super("The operation timed out.");
+    this.name = "TimeoutError";
+  }
+}
