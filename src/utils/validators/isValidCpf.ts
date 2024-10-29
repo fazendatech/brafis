ff62e1418ed788ad7c62a9aa949fc7034e735c5c
gabrieljablonski <contact@gabrieljablonski.com>
2024-10-29 09:52:52 -0300

    chore: update validator functions

---
+++ src/utils/validators/isValidCpf.ts
@@ -0,0 +1,46 @@
+function calcVerifierDigit(digits: string): number {
+  let factor = 10 - digits.length;
+  let sum = 0;
+
+  for (const d of digits) {
+    sum += Number(d) * factor;
+    factor += 1;
+    if (factor === 10) {
+      factor = 2;
+    }
+  }
+
+  const mod = sum % 11;
+  return mod === 10 ? 0 : mod;
+}
+
+/**
+ * @description Verifica se um CPF é válido.
+ *
+ * @param cnpj
+ * @param options
+ * @returns
+ */
+export function isValidCpf(
+  cpf: string,
+  options?: { strict?: boolean },
+): boolean {
+  const regex = options?.strict ? /[.-]/g : /[^\d]/g;
+  const stripped = cpf.replace(regex, "");
+
+  if (stripped.length !== 11) {
+    return false;
+  }
+  if (new Set(stripped).size === 1) {
+    return false;
+  }
+  if (stripped.startsWith("123456789")) {
+    return false;
+  }
+
+  let cpfGen = stripped.slice(0, 9);
+  cpfGen += `${calcVerifierDigit(cpfGen)}`;
+  cpfGen += `${calcVerifierDigit(cpfGen)}`;
+
+  return cpfGen === stripped;
+}
---
+++ src/utils/validators/isValidIe.test.ts
@@ -0,0 +1,9 @@
+import { describe, test } from "bun:test";
+
+describe("isValidCpf", () => {
+  test.todo(
+    "Implementar verdadeiras regras de validação para IE (varia por estado)",
+  );
+
+  test.todo("Handles `strict` option");
+});
