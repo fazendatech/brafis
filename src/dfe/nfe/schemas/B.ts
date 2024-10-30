415ab45b7389d5dd749ef74b19aca872c38e49cc
CayoPOliveira <cayoproliveira@gmail.com>
2024-10-30 00:36:13 -0300

    feat: create nfe schemas - groups A, B, BA and C

---
+++ src/dfe/nfe/schemas/B.ts
@@ -0,0 +1,72 @@
+import { z } from "zod";
+
+import { zCustom } from "@/utils/zCustom";
+
+import { schemaNfeNfRef } from "./BA";
+
+export const zUfCode = () =>
+  z.enum([
+    "12",
+    "27",
+    "16",
+    "13",
+    "29",
+    "23",
+    "53",
+    "32",
+    "52",
+    "21",
+    "51",
+    "50",
+    "31",
+    "15",
+    "25",
+    "41",
+    "26",
+    "22",
+    "33",
+    "24",
+    "43",
+    "11",
+    "14",
+    "42",
+    "35",
+    "28",
+    "17",
+  ]);
+
+const schemaNfeIde = z
+  .object({
+    cUF: zUfCode().describe("B02"),
+    cNF: zCustom.string.numeric().length(8).describe("B03"), // TODO: Validar
+    natOp: zCustom.string.range(1, 60).describe("B04"), // TODO: Validar
+    mod: z.enum(["55", "65"]).describe("B06"),
+    serie: zCustom.string.numeric().length(3).describe("B07"),
+    nNF: zCustom.string.numeric().min(1).max(9).describe("B08"),
+    dhEmi: zCustom.string.date().describe("B09"),
+    dhSaiEnt: zCustom.string.date().describe("B10"),
+    tpNF: z.enum(["0", "1"]).describe("B11"),
+    idDest: z.enum(["1", "2", "3"]).describe("B11a"),
+    cMunFG: zCustom.string.numeric().length(7).describe("B12"),
+    tpImp: z.enum(["0", "1", "2", "3", "4", "5"]).describe("B21"),
+    tpEmis: z.enum(["1", "2", "3", "4", "5", "6", "7", "9"]).describe("B22"),
+    cDV: z.number().nonnegative().lt(10).describe("B23"), // TODO: TENTAR ADICIONAR O ALGORITMO PARA PREENCHER AUTOMÁTICO
+    tpAmb: z.enum(["1", "2"]).describe("B24"),
+    finNFe: z.enum(["1", "2", "3", "4"]).describe("B25"),
+    indFinal: z.enum(["0", "1"]).describe("B25a"),
+    indPres: z.enum(["0", "1", "2", "3", "4", "5", "9"]).describe("B25b"),
+    indIntermed: z.enum(["0", "1"]).optional().describe("B25c"),
+    procEmi: z.enum(["0", "1", "2", "3"]).describe("B26"),
+    verProc: zCustom.string.range(1, 20).describe("B27"),
+    dhCont: zCustom.string.date().optional().describe("B28"),
+    xJust: zCustom.string.range(15, 256).optional().describe("B29"),
+    NFref: z.array(schemaNfeNfRef).max(500).optional(),
+  })
+  .describe("ide:B01");
+
+type NfeIde = z.infer<typeof schemaNfeIde>;
+
+/**
+ * @description Grupo B. Identificação da Nota Fiscal eletrônica
+ */
+export { schemaNfeIde, type NfeIde };
---
+++ src/dfe/nfe/schemas/BA.ts
@@ -0,0 +1,75 @@
+import { zCustom } from "@/utils/zCustom";
+
+import { z } from "zod";
+
+import { zUfCode } from "./B";
+
+const isValidAamm = (aamm: string) => {
+  const month = Number.parseInt(aamm.slice(2, 4));
+  return month >= 1 && month <= 12;
+};
+const schemaAAMM = () =>
+  zCustom.string
+    .numeric()
+    .length(4)
+    .refine((value) => isValidAamm(value));
+
+const schemaSerie = () => zCustom.string.numeric().min(1).max(3); // Informar '0' se não utilizada série
+const schemaNnf = () => zCustom.string.numeric().min(1).max(9); // Faixa: 1–999999999
+
+const schemaRefNF = z.object({
+  cUF: zUfCode().describe("BA04"),
+  AAMM: schemaAAMM().describe("BA05"),
+  CNPJ: zCustom.string.cnpj().describe("BA06"),
+  mod: z.enum(["01", "02"]).describe("BA07"), // 01=modelo 01; 02=modelo 02 (incluído na NT2016.002)
+  serie: schemaSerie().describe("BA08"),
+  nNF: schemaNnf().describe("BA09"),
+});
+
+const schemaRefNFP = z
+  .object({
+    cUF: zUfCode().describe("BA11"),
+    AAMM: schemaAAMM().describe("BA12"),
+    CNPJ: zCustom.string.cnpj().optional().describe("BA13"),
+    CPF: zCustom.string.cpf().optional().describe("BA14"),
+    IE: zCustom.string.ie().describe("BA15"),
+    mod: z.enum(["04", "01"]).describe("BA16"), //04=NF de Produtor; 01=NF (v2.0)
+    serie: schemaSerie().describe("BA17"),
+    nNF: schemaNnf().describe("BA18"),
+  })
+  .refine((obj) => zCustom.utils.hasOnlyOne([obj.CNPJ, obj.CPF]));
+
+const schemaRefECF = z.object({
+  // "2B"=Cupom Fiscal emitido por máquina registradora (não ECF);
+  // "2C"=Cupom Fiscal PDV;
+  // "2D"=Cupom Fiscal (emitido por ECF) (v2.0).
+  mod: z.enum(["2B", "2C", "2D"]).describe("BA21"),
+  nECF: zCustom.string.numeric().length(3).describe("BA22"),
+  nCOO: zCustom.string.numeric().length(6).describe("BA23"),
+});
+
+const schemaNfeNfRef = z
+  .object({
+    refNfe: zCustom.string.numeric().length(44).optional().describe("BA02"),
+    refNF: schemaRefNF.optional().describe("BA03"),
+    refNFP: schemaRefNFP.optional().describe("BA10"),
+    refCTe: zCustom.string.numeric().length(44).optional().describe("BA19"),
+    refECF: schemaRefECF.optional().describe("BA20"),
+  })
+  .refine((obj) =>
+    zCustom.utils.hasOnlyOne([
+      obj.refNfe,
+      obj.refNF,
+      obj.refNFP,
+      obj.refCTe,
+      obj.refECF,
+    ]),
+  )
+  .describe("NFref:BA01");
+
+type NfeNfRef = z.infer<typeof schemaNfeNfRef>;
+
+/**
+ * @description Grupo BA. Documento Fiscal Referenciado
+ */
+export { schemaNfeNfRef, type NfeNfRef };
