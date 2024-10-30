d73fe7c7533f2d0fd457290829e6c416a4cce504
CayoPOliveira <cayoproliveira@gmail.com>
2024-10-30 16:38:41 -0300

    refactor: nfe group file names

--- src/dfe/nfe/schemas/groupBA.ts
+++ src/dfe/nfe/schemas/groupBA.ts
@@ -2,7 +2,7 @@ import { zCustom } from "@/utils/zCustom";
 
 import { z } from "zod";
 
-import { zUfCode } from "./GroupB";
+import { zUfCode } from "./groupB";
 
 const isValidAamm = (aamm: string) => {
   const month = Number.parseInt(aamm.slice(2, 4));
