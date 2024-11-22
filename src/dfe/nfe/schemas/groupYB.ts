85a2948dd21e16b6c0e7cb3a7f0a66a8e0f06898
CayoPOliveira <cayoproliveira@gmail.com>
2024-11-22 00:12:39 -0300

    feat: adicionar esquemas para os grupos Y, YA e YB na NFe

---
+++ src/dfe/nfe/schemas/groupYB.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+import { zCustom } from "@/utils/zCustom";
+
+const schemaNfeInfIntermed = z
+  .object({
+    CNPJ: zCustom.string.cnpj().describe("YB02"),
+    idCadIntTran: z.string().length(60).describe("YB03"),
+  })
+  .describe("infIntermed:YB01");
+
+/**
+ * @description Grupo YB. Informações do Intermediador da Transação
+ */
+type NfeInfIntermed = z.infer<typeof schemaNfeInfIntermed>;
+
+export { schemaNfeInfIntermed, type NfeInfIntermed };
