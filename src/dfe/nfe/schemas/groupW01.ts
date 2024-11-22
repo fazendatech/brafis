6426f3a4166c86969f50f9904216948127ea9d1c
CayoPOliveira <cayoproliveira@gmail.com>
2024-11-21 22:27:17 -0300

    feat: add schemas for NFe group W, W01, and W02

---
+++ src/dfe/nfe/schemas/groupW01.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+import { zCustom } from "@/utils/zCustom";
+
+const schemaNfeISSQNTot = z
+  .object({
+    vServ: zCustom.string.decimal().length(15).optional().describe("W18"),
+    vBC: zCustom.string.decimal().length(15).optional().describe("W19"),
+    vISS: zCustom.string.decimal().length(15).optional().describe("W20"),
+    vPIS: zCustom.string.decimal().length(15).optional().describe("W21"),
+    vCOFINS: zCustom.string.decimal().length(15).optional().describe("W22"),
+    dCompet: zCustom.string.date().describe("W22a"),
+    vDeducao: zCustom.string.decimal().length(15).optional().describe("W22b"),
+    vOutro: zCustom.string.decimal().length(15).optional().describe("W22c"),
+    vDescIncond: zCustom.string
+      .decimal()
+      .length(15)
+      .optional()
+      .describe("W22d"),
+    vDescCond: zCustom.string.decimal().length(15).optional().describe("W22e"),
+    vISSRet: zCustom.string.decimal().length(15).optional().describe("W22f"),
+    cRegTrib: z
+      .enum(["1", "2", "3", "4", "5", "6"])
+      .optional()
+      .describe("W22g"),
+  })
+  .describe("ISSQNtot:W17");
+
+/**
+ * @description Grupo W01. Total da NF-e / ISSQN
+ */
+type NfeISSQNTot = z.infer<typeof schemaNfeISSQNTot>;
+
+export { schemaNfeISSQNTot, type NfeISSQNTot };
---
+++ src/dfe/nfe/schemas/groupw02.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+import { zCustom } from "@/utils/zCustom";
+
+const schemaNfeRetTrib = z
+  .object({
+    vRetPIS: zCustom.string.decimal().length(15).optional().describe("W31"),
+    vRetCOFINS: zCustom.string.decimal().length(15).optional().describe("W32"),
+    vRetCSLL: zCustom.string.decimal().length(15).optional().describe("W33"),
+    vBCIRRF: zCustom.string.decimal().length(15).optional().describe("W34"),
+    vIRRF: zCustom.string.decimal().length(15).optional().describe("W35"),
+    vBCRetPrev: zCustom.string.decimal().length(15).optional().describe("W36"),
+    vRetPrev: zCustom.string.decimal().length(15).optional().describe("W37"),
+  })
+  .describe("retTrib:W30");
+
+/**
+ * @description Grupo W02. Total da NF-e / Retenção de Tributos
+ */
+type NfeRetTrib = z.infer<typeof schemaNfeRetTrib>;
+
+export { schemaNfeRetTrib, type NfeRetTrib };
