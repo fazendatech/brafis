26d0e1a8ab79ff7d93cad1746d7f2ee98f96098c
CayoPOliveira <cayoproliveira@gmail.com>
2024-11-27 02:20:35 -0300

    fix: simplify validation logic for cMun, xMun, and UF in group G schema to improve readability

--- src/dfe/nfe/layoutNfe/groupG.ts
+++ src/dfe/nfe/layoutNfe/groupG.ts
@@ -15,25 +15,26 @@ const schemaNfeEntrega = z
     xMun: zCustom.string.range(2, 60).describe("G08"),
     UF: zUf().or(z.literal("EX")).describe("G09"),
     CEP: zCustom.string.numeric().length(8).optional().describe("G10"),
-    cPais: zCustom.string.numeric().length(4).optional().describe("G11"), // Utilizar a Tabela do BACEN (Seção 8.3 do MOC – Visão Geral,Tabela de UF, Município e País).
+    cPais: zCustom.string.numeric().length(4).optional().describe("G11"),
     xPais: zCustom.string.range(2, 60).optional().describe("G12"),
     fone: zCustom.string.phone().optional().describe("G13"),
     email: zCustom.string.range(1, 60).email().optional().describe("G14"),
     IE: zCustom.string.ie().optional().describe("G15"),
   })
+  .refine(({ CNPJ, CPF }) => zCustom.utils.hasOnlyOne([CNPJ, CPF]), {
+    message: "Deve ser informado apenas um dos campos: CNPJ ou CPF.",
+  })
   .refine(
     ({ cMun, xMun, UF }) => {
-      if (cMun === "9999999" && xMun === "EXTERIOR" && UF === "EX") {
-        return true;
-      }
-      if (cMun !== "9999999" && xMun !== "EXTERIOR" && UF !== "EX") {
-        return true;
-      }
-      return false;
+      const isExterior =
+        cMun === "9999999" && xMun === "EXTERIOR" && UF === "EX";
+      const isNotExterior =
+        cMun !== "9999999" && xMun !== "EXTERIOR" && UF !== "EX";
+      return isExterior || isNotExterior;
     },
     {
       message:
-        "Se a operação for no exterior, informar cMun = '9999999', xMun = 'EXTERIOR' e UF = 'EX",
+        "Se a operação for no exterior, informar cMun = '9999999', xMun = 'EXTERIOR' e UF = 'EX'.",
     },
   )
   .describe("entrega:G01");
