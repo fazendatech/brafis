e058194f31e418319b4ef57f29c62e127c4d7843
gabrieljablonski <contact@gabrieljablonski.com>
2024-11-27 09:45:38 -0300

    chore: validate cDV

--- src/dfe/nfe/layoutNfe/groupB.ts
+++ src/dfe/nfe/layoutNfe/groupB.ts
@@ -47,7 +47,7 @@ const schemaNfeIde = z
     cMunFG: zCustom.numeric().length(7).describe("B12"),
     tpImp: z.enum(["0", "1", "2", "3", "4", "5"]).describe("B21"),
     tpEmis: z.enum(["1", "2", "3", "4", "5", "6", "7", "9"]).describe("B22"),
-    cDV: z.number().nonnegative().lt(10).describe("B23"),
+    cDV: zCustom.numeric().length(1).describe("B23"),
     tpAmb: z.enum(["1", "2"]).describe("B24"),
     finNFe: z.enum(["1", "2", "3", "4"]).describe("B25"),
     indFinal: z.enum(["0", "1"]).describe("B25a"),
