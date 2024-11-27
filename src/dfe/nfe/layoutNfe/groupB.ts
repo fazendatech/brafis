b75d17767a53c6b425486bf0566420ce1ae0ac9f
CayoPOliveira <cayoproliveira@gmail.com>
2024-11-27 03:12:07 -0300

    chore: implement verifier digit calculation and update validation logic in group A and B schemas

--- src/dfe/nfe/layoutNfe/groupB.ts
+++ src/dfe/nfe/layoutNfe/groupB.ts
@@ -1,7 +1,30 @@
 import { z } from "zod";
 import { zCustom } from "@/utils/zCustom";
-import { invalidCnfs, zUfCode } from ".";
+import { zUfCode } from ".";
 import { schemaNfeNfRef } from "./groupBA";
+
+const invalidCnfs = new Set([
+  "00000000",
+  "11111111",
+  "22222222",
+  "33333333",
+  "44444444",
+  "55555555",
+  "66666666",
+  "77777777",
+  "88888888",
+  "99999999",
+  "12345678",
+  "23456789",
+  "34567890",
+  "45678901",
+  "56789012",
+  "67890123",
+  "78901234",
+  "89012345",
+  "90123456",
+  "01234567",
+]);
 
 const schemaNfeIde = z
   .object({
