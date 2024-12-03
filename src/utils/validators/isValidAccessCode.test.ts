2e8a2483d95d9d2552d93ca7fa984c0be9fcda97
CayoPOliveira <cayoproliveira@gmail.com>
2024-12-03 00:34:39 -0300

    test: add validation tests for isValidAccessCode with various scenarios

--- src/utils/validators/isValidAccessCode.test.ts
+++ src/utils/validators/isValidAccessCode.test.ts
@@ -3,5 +3,24 @@ import { describe, test, expect } from "bun:test";
 import { isValidAccessCode } from "./isValidAccessCode";
 
 describe("isValidAccessCode", () => {
-  test.todo("should return true for valid access codes");
+  const validAccessCode = "32191105570714000825550010059146621133082968";
+  test("valid access codes with lenght 44", () => {
+    expect(isValidAccessCode(validAccessCode.slice(0, -1))).toBeFalse(); //43
+    expect(isValidAccessCode(validAccessCode)).toBeTrue(); // 44
+    expect(isValidAccessCode(`${validAccessCode}0`)).toBeFalse(); //45
+  });
+
+  test("return false to access codes with invalid verifier digit", () => {
+    expect(isValidAccessCode(`${validAccessCode.slice(0, -1)}0`)).toBeFalse();
+  });
+
+  test("Handles `strict` option", () => {
+    expect(
+      isValidAccessCode(`[a?@]_ ${validAccessCode}`, { strict: false }),
+    ).toBeTrue();
+    expect(isValidAccessCode(validAccessCode, { strict: true })).toBeTrue();
+    expect(
+      isValidAccessCode(`[a?@]_ ${validAccessCode}`, { strict: true }),
+    ).toBeFalse();
+  });
 });
