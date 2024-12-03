5d8c4ac8b9b5880bd55f7d0b86e78353956cfeeb
CayoPOliveira <cayoproliveira@gmail.com>
2024-12-03 00:02:07 -0300

    test: implement validation tests for isValidIe function with various scenarios

--- src/utils/validators/isValidIe.test.ts
+++ src/utils/validators/isValidIe.test.ts
@@ -1,9 +1,20 @@
-import { describe, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
+import { isValidIe } from "./isValidIe";
 
-describe("isValidCpf", () => {
-  test.todo(
-    "Implementar verdadeiras regras de validação para IE (varia por estado)",
-  );
+describe("isValidIe", () => {
+  test("returns true for valid IE with length between 8 and 14", () => {
+    expect(isValidIe("12345678")).toBeTrue();
+    expect(isValidIe("12345678901234")).toBeTrue();
+  });
 
-  test.todo("Handles `strict` option");
+  test("returns false for IE with length less than 8 or greater than 14", () => {
+    expect(isValidIe("1234567")).toBeFalse();
+    expect(isValidIe("123456789012345")).toBeFalse();
+  });
+
+  test("validate strict option", () => {
+    expect(isValidIe("[a?@] 12.345.678-9", { strict: false })).toBeTrue();
+    expect(isValidIe("12.345.678-9", { strict: true })).toBeTrue();
+    expect(isValidIe("[a?@] 12.345.678-9", { strict: true })).toBeFalse();
+  });
 });
