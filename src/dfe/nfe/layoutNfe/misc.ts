02c0eaced483f38a419231f38e199e689a224407
CayoPOliveira <cayoproliveira@gmail.com>
2024-12-09 14:07:24 -0300

    fix: correct ufCodeList and ufList assignments

--- src/dfe/nfe/layoutNfe/misc.ts
+++ src/dfe/nfe/layoutNfe/misc.ts
@@ -3,10 +3,10 @@ import { ufCodeMap } from "@/ufCode";
 import type { UF, UFCode } from "@/ufCode/types";
 
 // NOTE: Hack pro zod aceitar tipos customizados (https://stackoverflow.com/a/73825370)
-const ufCodeList = Object.keys(ufCodeMap);
+const ufCodeList = Object.values(ufCodeMap);
 const ufCodes: [UFCode, ...UFCode[]] = [ufCodeList[0], ...ufCodeList];
 export const zUfCode = () => z.enum(ufCodes);
 
-const ufList = Object.values(ufCodeMap);
+const ufList = Object.keys(ufCodeMap);
 const ufs: [UF, ...UF[]] = [ufList[0], ...ufList];
 export const zUf = () => z.enum(ufs);
