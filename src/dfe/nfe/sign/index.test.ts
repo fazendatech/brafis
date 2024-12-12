015a5bc72a508a6a6be9cb3398de2889d42e65a8
CayoPOliveira <cayoproliveira@gmail.com>
2024-12-12 16:08:19 -0300

    feat: implement signNfe function to generate signed XML for NFe

---
+++ src/dfe/nfe/sign/index.test.ts
@@ -0,0 +1,44 @@
+import { describe, expect, test } from "bun:test";
+import { CertificateP12 } from "@/certificate";
+import { signNfe } from ".";
+import { NFE_TEST_DATA } from "@/dfe/nfe/layout/misc";
+import { XMLBuilder, XMLParser } from "fast-xml-parser";
+
+describe("sign", () => {
+  describe("signNfe", () => {
+    test("Returns true when generate and sign the XML correctly", async () => {
+      expect(
+        signNfe(
+          NFE_TEST_DATA,
+          await CertificateP12.fromFilepath({
+            filepath: process.env.TEST_CERTIFICATE_PATH ?? "",
+            password: process.env.TEST_CERTIFICATE_PASSWORD ?? "",
+          }),
+        ),
+      ).toMatchSnapshot();
+    });
+
+    test("É possível fazer o parse e build novamente da nfe assinada", async () => {
+      const parser = new XMLParser({
+        parseTagValue: false,
+        ignoreAttributes: false,
+        attributeNamePrefix: "@_",
+      });
+      const builder = new XMLBuilder({
+        attributeNamePrefix: "@_",
+        suppressEmptyNode: true,
+        ignoreAttributes: false,
+      });
+
+      const signedNfe = signNfe(
+        NFE_TEST_DATA,
+        await CertificateP12.fromFilepath({
+          filepath: process.env.TEST_CERTIFICATE_PATH ?? "",
+          password: process.env.TEST_CERTIFICATE_PASSWORD ?? "",
+        }),
+      );
+      const signedNfeObject = parser.parse(signedNfe);
+      expect(builder.build(signedNfeObject)).toEqual(signedNfe);
+    });
+  });
+});
---
+++ src/dfe/nfe/sign/index.ts
@@ -0,0 +1,41 @@
+import { XMLBuilder } from "fast-xml-parser";
+import { SignedXml } from "xml-crypto";
+import { parseNfe, type NfeLayout } from "../layout";
+import type { CertificateP12 } from "@/certificate";
+
+/**
+ * @description Gera o XML assinado da NFe.
+ *
+ * @param nfe - NFe a ser assinada.
+ * @param certificate - O certificado usado na assinatura.
+ */
+export function signNfe(nfe: NfeLayout, certificate: CertificateP12): string {
+  parseNfe(nfe);
+  const { key, cert } = certificate.asPem();
+
+  const builder = new XMLBuilder({
+    ignoreAttributes: false,
+    attributeNamePrefix: "@_",
+  });
+  const xml = builder.build(nfe);
+
+  const sig = new SignedXml({
+    privateKey: key,
+    publicCert: cert,
+    signatureAlgorithm: "http://www.w3.org/2000/09/xmldsig#rsa-sha1",
+    canonicalizationAlgorithm:
+      "http://www.w3.org/TR/2001/REC-xml-c14n-20010315",
+  });
+  sig.addReference({
+    xpath: "//*[local-name(.)='infNFe']",
+    uri: nfe.NFe.infNFe["@_Id"],
+    transforms: [
+      "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
+      "http://www.w3.org/TR/2001/REC-xml-c14n-20010315",
+    ],
+    digestAlgorithm: "http://www.w3.org/2000/09/xmldsig#sha1",
+  });
+
+  sig.computeSignature(xml);
+  return sig.getSignedXml();
+}
