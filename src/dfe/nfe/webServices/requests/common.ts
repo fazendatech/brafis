6f17ab5302942553f680cc8a07b3498895d6083b
CayoPOliveira <cayoproliveira@gmail.com>
2024-12-26 19:47:02 -0300

    feat(nfe): add Zod schema validation for NFe request options and implement tests

--- src/dfe/nfe/webServices/requests/common.ts
+++ src/dfe/nfe/webServices/requests/common.ts
@@ -1,7 +1,39 @@
-export interface NfeRequestOptions<Body> {
-  body: Body;
-  timeout: number;
-}
+import { z } from "zod";
+
+// export interface NfeRequestOptions<Body> {
+//   body: Body;
+//   timeout: number;
+//   sign?: { xpath?: string; id?: string };
+// }
+
+export const schemaNfeRequestOptions = z
+  .object({
+    body: z.any(),
+    timeout: z.number(),
+    sign: z
+      .object({
+        xpath: z.string().optional(),
+        id: z.string().optional(),
+      })
+      .optional(),
+  })
+  .refine(
+    ({ sign }) => {
+      if (!sign) {
+        return true;
+      }
+      if (sign.xpath && !sign.id) {
+        return true;
+      }
+      if (!sign.xpath && sign.id) {
+        return true;
+      }
+      return false;
+    },
+    { message: "sign must have either xpath or id" },
+  );
+
+export type NfeRequestOptions = z.infer<typeof schemaNfeRequestOptions>;
 
 export interface NfeWebServiceResponse<Status, Raw> {
   status: Status | "outro";
