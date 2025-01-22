e38f1dde7bc67d679538fad9053623d7d1c76332
gabrieljablonski <contact@gabrieljablonski.com>
2025-01-22 15:37:49 -0300

    chore: improve types for consultaProtocolo

--- src/dfe/nfe/webServices/requests/consultaProtocolo.ts
+++ src/dfe/nfe/webServices/requests/consultaProtocolo.ts
@@ -1,9 +1,17 @@
 import type { UFCode } from "@/ufCode/types";
-import type { WithXmlns, WithXmlnsVersao } from "@/utils/soap/types";
+import type {
+  WithVersao,
+  WithXmlns,
+  WithXmlnsVersao,
+} from "@/utils/soap/types";
 import type { LiteralStringUnion } from "@/utils/types";
 
 import type { NfeWebServiceResponse } from "./common";
-import type { ProtNFe } from "./autorizacao";
+import type { NfeAutorizacaoProtNfe } from "./autorizacao";
+import type {
+  NfeRecepcaoEventoEventoWithSignature,
+  NfeRecepcaoEventoRetEvento,
+} from "@/dfe/nfe/webServices/requests/recepcaoEvento";
 
 /**
  * @description Opções para configurar o web service de NFe Consulta Protocolo.
@@ -22,6 +30,11 @@ export type NfeConsultaProtocoloRequest = WithXmlns<{
   }>;
 }>;
 
+type ProcEventoNfe = {
+  evento: NfeRecepcaoEventoEventoWithSignature;
+  retEvento: NfeRecepcaoEventoRetEvento;
+};
+
 /**
  * @description Resposta completa da consulta de status serviço.
  *
@@ -36,14 +49,9 @@ export type NfeConsultaProtocoloRequest = WithXmlns<{
  * • Informar se localizada uma NF-e com cStat = 100-
 uso autorizado, 150-uso autorizado fora de prazo ou
 110-uso denegado. (NT 2012.003)
-  * @property [retCancNFe] - Informações de cancelamento da NF-e.
-Informar se localizada uma
-NF-e com cStat = 101-cancelado ou 151-
-cancelado fora de prazo. (NT 2012.003)
   * @property [procEventoNFe] - Informação do evento e respectivo protocolo do evento.
  */
-export interface NfeConsultaProtocoloResponseRaw {
-  "@_versao": string;
+export type NfeConsultaProtocoloResponseRaw = WithVersao<{
   tpAmb: "1" | "2";
   verAplic: string;
   cStat: LiteralStringUnion<"100" | "101" | "110">;
@@ -51,10 +59,9 @@ export interface NfeConsultaProtocoloResponseRaw {
   cUF: UFCode;
   dhRecbto: string;
   chNFe: string;
-  protNFe?: ProtNFe;
-  retCancNFe?: unknown;
-  procEventoNFe?: { "@_versao": string; evento: unknown; retEvento: unknown }[];
-}
+  protNFe?: NfeAutorizacaoProtNfe;
+  procEventoNFe?: ProcEventoNfe[];
+}>;
 
 export type NfeConsultaProtocoloStatus =
   | "uso-autorizado"
