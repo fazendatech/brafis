ca686259869c4e9eb67a5edaefe257bd0c408270
CayoPOliveira <cayoproliveira@gmail.com>
2025-01-27 21:35:03 -0300

    refactor(nfe): remove xmlNamespace property and update usage in NfeWebServices

--- src/utils/soap/types.ts
+++ src/utils/soap/types.ts
@@ -1,4 +1,17 @@
-export type WithXmlns<T = unknown> = { "@_xmlns": string } & T;
+import type { LiteralStringUnion } from "../types";
+
+export type WithXmlns<T = unknown> = {
+  "@_xmlns": LiteralStringUnion<
+    | "http://www.portalfiscal.inf.br/nfe"
+    | "http://www.portalfiscal.inf.br/nfe/wsdl/NFeStatusServico4"
+    | "http://www.portalfiscal.inf.br/nfe/wsdl/CadConsultaCadastro4"
+    | "http://www.portalfiscal.inf.br/nfe/wsdl/NFeAutorizacao4"
+    | "http://www.portalfiscal.inf.br/nfe/wsdl/NFeInutilizacao4"
+    | "http://www.portalfiscal.inf.br/nfe/wsdl/NFeRecepcaoEvento4"
+    | "http://www.portalfiscal.inf.br/nfe/wsdl/NFeConsultaProtocolo4"
+    | "http://www.portalfiscal.inf.br/nfe/wsdl/NFeDistribuicaoDFe"
+  >;
+} & T;
 
 export type WithVersao<T = unknown> = { "@_versao": string } & T;
 
