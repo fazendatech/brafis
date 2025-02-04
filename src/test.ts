c98a2050a8e0b6d6219e98bdd89d91b296aab143
CayoPOliveira <cayoproliveira@gmail.com>
2025-02-04 01:29:11 -0300

    chore: restore test.ts and update examples of use

--- src/test.ts
+++ src/test.ts
@@ -13,20 +13,29 @@ const service = new NfeWebServices({
 });
 
 const statusServ = await service.statusServico();
+
+// biome-ignore lint/suspicious/noConsoleLog: This is a test file
+// biome-ignore lint/suspicious/noConsole: This is a test file
 console.log(JSON.stringify(statusServ, null, 2));
 
 const consCad = await service.consultaCadastro({
   CPF: process.env.TEST_CPF ?? "",
 });
+// biome-ignore lint/suspicious/noConsoleLog: This is a test file
+// biome-ignore lint/suspicious/noConsole: This is a test file
 console.log(JSON.stringify(consCad, null, 2));
 
 const consProt = await service.consultaProtocolo({
   chNFe: process.env.TEST_CHNFE ?? "",
 });
+// biome-ignore lint/suspicious/noConsoleLog: This is a test file
+// biome-ignore lint/suspicious/noConsole: This is a test file
 console.log(JSON.stringify(consProt, null, 2));
 
 const dfe = await service.distribuicaoDfe({
   CPF: process.env.TEST_CERTIFICATE_CPF ?? "",
   consChNFe: { chNFe: process.env.TEST_CHNFE ?? "" },
 });
+// biome-ignore lint/suspicious/noConsoleLog: This is a test file
+// biome-ignore lint/suspicious/noConsole: This is a test file
 console.log(JSON.stringify(dfe, null, 2));
